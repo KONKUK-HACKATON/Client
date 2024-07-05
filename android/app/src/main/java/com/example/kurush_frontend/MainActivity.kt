edb2f9aec9217bab11bfc6e7e7c2c34b250b86e5
hwidung <hwhwhw74@naver.com>
2024-07-05 21:45:45 +0900

    design: 학번 성별 뷰

--- android/app/src/main/java/com/example/kurush_frontend/MainActivity.kt
+++ android/app/src/main/java/com/example/kurush_frontend/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.kurush_frontend.databinding.ActivityMainBinding
 import com.example.kurush_frontend.initialscreen.departmentFragment
+import com.example.kurush_frontend.initialscreen.studennumFragment
 import com.example.kurush_frontend.matching.main.MatchingMainFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
@@ -19,9 +20,9 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-//        supportFragmentManager.beginTransaction()
-//            .replace(R.id.main, departmentFragment())
-//            .commit()
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.main, studennumFragment())
+            .commit()
 
 //        supportFragmentManager.beginTransaction().replace(R.id.main_frm, )
 //            .commitAllowingStateLoss()
