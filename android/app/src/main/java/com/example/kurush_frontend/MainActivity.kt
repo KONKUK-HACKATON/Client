987c68674d6aa7d6317e95ff86c6d0f07451d5b3
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2024-07-06 00:40:54 +0900

    feat : 하단 네비 이동 기능

--- android/app/src/main/java/com/example/kurush_frontend/MainActivity.kt
+++ android/app/src/main/java/com/example/kurush_frontend/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.kurush_frontend
 
 import android.app.Activity
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,6 +11,7 @@ import com.example.kurush_frontend.databinding.ActivityMainBinding
 import com.example.kurush_frontend.initialscreen.departmentFragment
 import com.example.kurush_frontend.initialscreen.eatingFragment
 import com.example.kurush_frontend.initialscreen.studennumFragment
+import com.example.kurush_frontend.matching.add.MatchingAddFragment
 import com.example.kurush_frontend.matching.main.MatchingMainFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
@@ -23,8 +25,33 @@ class MainActivity : AppCompatActivity() {
 
         supportFragmentManager.beginTransaction().replace(R.id.main_frm, MatchingMainFragment()).commit()
 
+        initBottomNavigation()
+
 //        supportFragmentManager.beginTransaction().replace(R.id.main_frm, )
 //            .commitAllowingStateLoss()
 
+    }
+
+    private fun initBottomNavigation() {
+        binding.bottomNavigationView.selectedItemId = R.id.fragment_addUser
+
+        binding.bottomNavigationView.setOnItemSelectedListener { item ->
+            when(item.itemId) {
+                R.id.fragment_calendar -> {
+                    //TODO calender fragment 생성
+                }
+                R.id.fragment_addUser -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_frm, MatchingMainFragment()).commit()
+                }
+                R.id.fragment_home -> {
+                    //TODO home fragment 생성
+                }
+                R.id.fragment_chat -> {
+                    //TODO chat fragment 생성.
+                }
+            }
+            true
+        }
     }
 }
