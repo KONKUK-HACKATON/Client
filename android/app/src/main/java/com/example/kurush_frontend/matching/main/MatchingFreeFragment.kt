cafbd023e09ffe005cbcea4924d024f6dbeff46a
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2024-07-05 18:51:12 +0900

    feat : 세로 리사뷰 완성

--- android/app/src/main/java/com/example/kurush_frontend/matching/main/MatchingFreeFragment.kt
+++ android/app/src/main/java/com/example/kurush_frontend/matching/main/MatchingFreeFragment.kt
@@ -5,10 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.kurush_frontend.data.HorizonData
+import com.example.kurush_frontend.data.VerticalData
 import com.example.kurush_frontend.databinding.FragmentMatchingFreeBinding
+import com.example.kurush_frontend.matching.main.adapter.HorizonRVAdapter
+import com.example.kurush_frontend.matching.main.adapter.VerticalRVAdapter
 
 class MatchingFreeFragment : Fragment() {
     lateinit var binding: FragmentMatchingFreeBinding
+    lateinit var dummyItemsHor : ArrayList<HorizonData>
+    lateinit var dummyItemsVer : ArrayList<VerticalData>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -16,9 +22,86 @@ class MatchingFreeFragment : Fragment() {
     ): View? {
         binding = FragmentMatchingFreeBinding.inflate(inflater, container, false)
 
-        binding.rvMatchingFreeHorizon.adapter =
+        initDummyData()
+        
+        binding.rvMatchingFreeHorizon.adapter = HorizonRVAdapter(dummyItemsHor)
+
+        binding.rvMatchingFreeVertical.adapter = VerticalRVAdapter(dummyItemsVer)
 
         return binding.root
+    }
+
+    private fun initDummyData() {
+        dummyItemsVer = ArrayList<VerticalData>()
+        dummyItemsHor = ArrayList<HorizonData>()
+        dummyItemsHor.add(
+            HorizonData("nickname", "department", "country", "gender")
+        )
+        dummyItemsHor.add(
+            HorizonData("nickname", "department", "country", "gender")
+        )
+        dummyItemsHor.add(
+            HorizonData("nickname", "department", "country", "gender")
+        )
+        dummyItemsHor.add(
+            HorizonData("nickname", "department", "country", "gender")
+        )
+        dummyItemsHor.add(
+            HorizonData("nickname", "department", "country", "gender")
+        )
+        dummyItemsHor.add(
+            HorizonData("nickname", "department", "country", "gender")
+        )
+        dummyItemsHor.add(
+            HorizonData("nickname", "department", "country", "gender")
+        )
+
+        dummyItemsVer.add(
+            VerticalData("nickname", "제목", "더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 "
+            , 5)
+        )
+        dummyItemsVer.add(
+            VerticalData("nickname", "제목", "더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 "
+                , 5)
+        )
+        dummyItemsVer.add(
+            VerticalData("nickname", "제목", "더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 "
+                , 5)
+        )
+        dummyItemsVer.add(
+            VerticalData("nickname", "제목", "더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 "
+                , 5)
+        )
+        dummyItemsVer.add(
+            VerticalData("nickname", "제목", "더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 "
+                , 5)
+        )
+        dummyItemsVer.add(
+            VerticalData("nickname", "제목", "더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 "
+                , 5)
+        )
+        dummyItemsVer.add(
+            VerticalData("nickname", "제목", "더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 "
+                , 5)
+        )
+        dummyItemsVer.add(
+            VerticalData("nickname", "제목", "더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 "
+                , 5)
+        )
+        dummyItemsVer.add(
+            VerticalData("nickname", "제목", "더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 "
+                , 5)
+        )
+        dummyItemsVer.add(
+            VerticalData("nickname", "제목", "더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 "
+                , 5)
+        )
+        dummyItemsVer.add(
+            VerticalData("nickname", "sdfs", "더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 더미데이터 "
+                , 5)
+        )
+
+        
     }
 
 }
